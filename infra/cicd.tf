9cb4c1d4df298330e2874db3f490ad31701a58ae
max <maxrahimi7@gmail.com>
2026-09-24 09:02:59 -0400

    Debug OIDC: inline the deploy role ARN and print token claims

--- infra/cicd.tf
+++ infra/cicd.tf
@@ -28,7 +28,7 @@ data "aws_iam_policy_document" "github_assume" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_repo}:ref:refs/heads/*"]
+      values   = ["repo:${var.github_repo}:*"]
     }
   }
 }
